698272bf54c03f55478ddf8971ef6ace84748d1d
Kacper Cömlek <kacper74@hotmail.com>
2022-12-17 12:05:21 +0100

    initial setup +tailwind +antd

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.tsx",
+
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
